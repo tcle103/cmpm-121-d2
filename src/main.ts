7c768c5b0e2b1b77a5960d50d90ff179e4d1a0db
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-10-17 02:28:14 +0000

    modified array of points to store arrays of point arrays to represent lines to support undo/redo, implemented undo/redo

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import "./style.css";
 
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 const clearButton: HTMLButtonElement = document.createElement("button");
+const undoButton: HTMLButtonElement = document.createElement("button");
+const redoButton: HTMLButtonElement = document.createElement("button");
 const ctx = canvas.getContext("2d");
 const cursor = { x: 0, y: 0 };
-const pointarr: number[][] = [];
+const pointarr: number[][][] = [];
+const redoarr: number[][][] = [];
 const drawChange: Event = new Event("drawing-changed");
 let drawFlag: boolean = false;
 
@@ -13,19 +16,27 @@ canvas.height = 256;
 canvas.width = 256;
 document.body.append(canvas);
 document.body.append(clearButton);
+document.body.append(undoButton);
+document.body.append(redoButton);
 
 canvas.addEventListener("pointerdown", (e) => {
   drawFlag = true;
   console.log("down!");
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+  pointarr.push([]);
 });
 canvas.addEventListener("pointerup", () => {
   drawFlag = false;
 });
 canvas.addEventListener("mousemove", (e) => {
   if (drawFlag) {
-    pointarr.push([cursor.x, cursor.y, e.offsetX, e.offsetY]);
+    pointarr[pointarr.length - 1].push([
+      cursor.x,
+      cursor.y,
+      e.offsetX,
+      e.offsetY,
+    ]);
     canvas.dispatchEvent(drawChange);
   }
 });
@@ -34,20 +45,23 @@ canvas.addEventListener("drawing-changed", () => {
   console.log("wah!");
   ctx?.clearRect(0, 0, 256, 256);
   ctx?.beginPath();
-  for (let i: number = 0; i < pointarr.length; ++i) {
-    const pt1: number[] = [
-      pointarr[i][0],
-      pointarr[i][1],
-    ];
-    const pt2: number[] = [
-      pointarr[i][2],
-      pointarr[i][3],
-    ];
-    ctx?.moveTo(pt1[0], pt1[1]);
-    ctx?.lineTo(pt2[0], pt2[1]);
-    ctx?.stroke();
-    cursor.x = pt2[0];
-    cursor.y = pt2[1];
+  for (let j: number = 0; j < pointarr.length; ++j) {
+    const line: number[][] = pointarr[j];
+    for (let i: number = 0; i < line.length; ++i) {
+      const pt1: number[] = [
+        line[i][0],
+        line[i][1],
+      ];
+      const pt2: number[] = [
+        line[i][2],
+        line[i][3],
+      ];
+      ctx?.moveTo(pt1[0], pt1[1]);
+      ctx?.lineTo(pt2[0], pt2[1]);
+      ctx?.stroke();
+      cursor.x = pt2[0];
+      cursor.y = pt2[1];
+    }
   }
 });
 
@@ -55,4 +69,26 @@ clearButton.innerHTML = "clear";
 clearButton.addEventListener("click", () => {
   ctx?.clearRect(0, 0, 256, 256);
   pointarr.splice(0, pointarr.length);
+});
+
+undoButton.innerHTML = "undo";
+undoButton.addEventListener("click", () => {
+  if (pointarr.length > 0) {
+    const temp: number[][] | undefined = pointarr.pop();
+    if (temp) {
+      redoarr.push(temp);
+    }
+    canvas.dispatchEvent(drawChange);
+  }
+});
+
+redoButton.innerHTML = "redo";
+redoButton.addEventListener("click", () => {
+  if (redoarr.length > 0) {
+    const temp: number[][] | undefined = redoarr.pop();
+    if (temp) {
+      pointarr.push(temp);
+    }
+    canvas.dispatchEvent(drawChange);
+  }
 });
