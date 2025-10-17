e36f315c1a74144993d054ee254cbf13ad1f9719
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-10-17 02:06:24 +0000

    implemented drawing thru a "drawing-change" event and storing points in array

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import "./style.css";
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 const clearButton: HTMLButtonElement = document.createElement("button");
 const ctx = canvas.getContext("2d");
-let drawFlag = false;
 const cursor = { x: 0, y: 0 };
+const pointarr: number[][] = [];
+const drawChange: Event = new Event("drawing-changed");
+let drawFlag: boolean = false;
 
 document.body.innerHTML += `<h1>draw</h1>`;
 canvas.height = 256;
@@ -23,13 +25,33 @@ canvas.addEventListener("pointerup", () => {
 });
 canvas.addEventListener("mousemove", (e) => {
   if (drawFlag) {
-    ctx?.beginPath();
-    ctx?.moveTo(cursor.x, cursor.y);
-    ctx?.lineTo(e.offsetX, e.offsetY);
-    ctx?.stroke();
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
+    pointarr.push([cursor.x, cursor.y, e.offsetX, e.offsetY]);
+    canvas.dispatchEvent(drawChange);
+    // ctx?.beginPath();
+    // ctx?.moveTo(cursor.x, cursor.y);
+    // ctx?.lineTo(e.offsetX, e.offsetY);
+    // ctx?.stroke();
+    // cursor.x = e.offsetX;
+    // cursor.y = e.offsetY;
   }
+});
+
+canvas.addEventListener("drawing-changed", () => {
+  console.log("wah!");
+  ctx?.beginPath();
+  const pt1: number[] = [
+    pointarr[pointarr.length - 1][0],
+    pointarr[pointarr.length - 1][1],
+  ];
+  const pt2: number[] = [
+    pointarr[pointarr.length - 1][2],
+    pointarr[pointarr.length - 1][3],
+  ];
+  ctx?.moveTo(pt1[0], pt1[1]);
+  ctx?.lineTo(pt2[0], pt2[1]);
+  ctx?.stroke();
+  cursor.x = pt2[0];
+  cursor.y = pt2[1];
 });
 
 clearButton.innerHTML = "clear";
