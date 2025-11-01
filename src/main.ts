5e134d9fbfff3779d75294f0368e53510973e810
Tien Le <146970839+tcle103@users.noreply.github.com>
2025-11-01 04:16:08 +0000

    added strokeWidth parameter to Line interface iterdraw/draw functions in prep for multiple markers

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 
+const thinStyle = 1;
+const thickStyle = 3;
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 const clearButton: HTMLButtonElement = document.createElement("button");
 const undoButton: HTMLButtonElement = document.createElement("button");
@@ -13,8 +15,16 @@ const redraw: Event = new Event("redraw");
 let drawFlag: boolean = false;
 interface Line {
   points: number[][];
-  display(ctx: CanvasRenderingContext2D | null, obj: Line): void;
-  drag(obj: Line): void;
+  display(
+    ctx: CanvasRenderingContext2D | null,
+    obj: Line,
+    strokeWidth: number,
+  ): void;
+  drag(
+    ctx: CanvasRenderingContext2D | null,
+    obj: Line,
+    strokeWidth: number,
+  ): void;
 }
 
 document.body.innerHTML += `<h1>draw</h1>`;
@@ -25,9 +35,14 @@ document.body.append(clearButton);
 document.body.append(undoButton);
 document.body.append(redoButton);
 
-function draw(ctx: CanvasRenderingContext2D | null, obj: Line): void {
+function draw(
+  ctx: CanvasRenderingContext2D | null,
+  obj: Line,
+  strokeWidth: number,
+): void {
   if (ctx) {
     console.log("drawing");
+    ctx.lineWidth = strokeWidth;
     ctx?.beginPath();
     for (let i: number = 0; i < obj.points.length; ++i) {
       const pt = obj.points[i];
@@ -40,13 +55,20 @@ function draw(ctx: CanvasRenderingContext2D | null, obj: Line): void {
   }
 }
 
-function iterDraw(obj: Line): void {
-  const pt = obj.points[obj.points.length - 1];
-  ctx?.moveTo(pt[0], pt[1]);
-  ctx?.lineTo(pt[2], pt[3]);
-  ctx?.stroke();
-  cursor.x = pt[2];
-  cursor.y = pt[3];
+function iterDraw(
+  ctx: CanvasRenderingContext2D | null,
+  obj: Line,
+  strokeWidth: number,
+): void {
+  if (ctx) {
+    const pt = obj.points[obj.points.length - 1];
+    ctx.lineWidth = strokeWidth;
+    ctx.moveTo(pt[0], pt[1]);
+    ctx.lineTo(pt[2], pt[3]);
+    ctx.stroke();
+    cursor.x = pt[2];
+    cursor.y = pt[3];
+  }
 }
 
 canvas.addEventListener("pointerdown", (e) => {
@@ -78,14 +100,14 @@ canvas.addEventListener("mousemove", (e) => {
 });
 
 canvas.addEventListener("drawing-changed", () => {
-  linearr[linearr.length - 1].drag(linearr[linearr.length - 1]);
+  linearr[linearr.length - 1].drag(ctx, linearr[linearr.length - 1], thinStyle);
 });
 
 canvas.addEventListener("redraw", () => {
   ctx?.clearRect(0, 0, 256, 256);
   console.log(linearr);
   for (let i: number = 0; i < linearr.length; ++i) {
-    linearr[i].display(ctx, linearr[i]);
+    linearr[i].display(ctx, linearr[i], thinStyle);
   }
 });
 
